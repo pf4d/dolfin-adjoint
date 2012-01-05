b34e774e548703cb092d3c310c83e827af2bc00d
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-01-05 14:18:35 +0000

    Taylor remainder convergence test for the adjoint.

--- tests/demo_heat.py
+++ tests/demo_heat.py
@@ -4,17 +4,16 @@ from dolfin_adjoint import *
 debugging["record_all"] = True
 
 f = Expression("x[0]*(x[0]-1)*x[1]*(x[1]-1)")
+mesh = UnitSquare(4, 4)
+V = FunctionSpace(mesh, "CG", 1)
 
-def run_forward(initial_condition="0.0", annotate=True, dump=True):
-  mesh = UnitSquare(4, 4)
-  V = FunctionSpace(mesh, "CG", 1)
-
+def run_forward(initial_condition=None, annotate=True, dump=True):
   u = TrialFunction(V)
   v = TestFunction(V)
 
   u_0 = Function(V)
-  ic_expression = Expression(initial_condition)
-  u_0.interpolate(ic_expression)
+  if initial_condition is not None:
+    u_0.assign(initial_condition)
 
   u_1 = Function(V)
 
@@ -70,27 +69,86 @@ def run_replay():
 run_replay()
 
 # The functional is only a function of final state.
-functional=Functional(u_0*dx)
+functional=Functional(u_0*u_0*dx)
 f_direct = adjointer.evaluate_functional(functional, adjointer.equation_count-1)
 
 print "Running adjoint model ..."
 
-z_out = File("adjoint.pvd", "compressed")
-f_adj=0.0
+def run_adjoint():
+  z_out = File("adjoint.pvd", "compressed")
+  f_adj=0.0
 
-for i in range(adjointer.equation_count)[::-1]:
+  for i in range(adjointer.equation_count)[::-1]:
 
-    (adj_var, output) = adjointer.get_adjoint_solution(i, functional)
-    
-    storage = libadjoint.MemoryStorage(output)
-    adjointer.record_variable(adj_var, storage)
+      (adj_var, output) = adjointer.get_adjoint_solution(i, functional)
+      
+      storage = libadjoint.MemoryStorage(output)
+      adjointer.record_variable(adj_var, storage)
 
-    z_out << output.data
+      z_out << output.data
 
-    if i!=0:
-        # f is only the RHS from equation 1 onwards. At equation 0, the
-        # RHS is the zero vector, so we don't bother. 
-        f_adj+=assemble(-f*output.data*dx)
+      if i!=0:
+          # f is only the RHS from equation 1 onwards. At equation 0, the
+          # RHS is the zero vector, so we don't bother. 
+          f_adj+=assemble(-f*output.data*dx)
 
-print "Evaluating functional only using adjoint ... "
-print "Difference between the functional evaluatons: ", f_adj-f_direct
+  return (f_adj, output.data)
+
+(f_adj, final_adjoint) = run_adjoint()
+
+#print "Evaluating functional only using adjoint ... "
+#print "Difference between the functional evaluatons (should be 0): ", f_adj-f_direct
+
+def convergence_order(errors):
+  import math
+
+  orders = [0.0] * (len(errors)-1)
+  for i in range(len(errors)-1):
+    orders[i] = math.log(errors[i]/errors[i+1], 2)
+
+  return orders
+
+
+def test_ic_gradient(final_adjoint):
+  # We will compute the gradient of the functional with respect to the initial condition,
+  # and check its correctness with the Taylor remainder convergence test.
+  print "Running Taylor remainder convergence analysis ... "
+  import random
+  import numpy
+
+  # Randomise the perturbation direction:
+  perturbation_direction = Function(V)
+  vec = perturbation_direction.vector()
+  for i in range(len(vec)):
+    vec[i] = random.random()
+
+  # Run the forward problem for various perturbed initial conditions
+  functional_values = []
+  perturbations = []
+  for perturbation_size in [0.001/(2**i) for i in range(5)]:
+    perturbation = Function(perturbation_direction)
+    vec = perturbation.vector()
+    vec *= perturbation_size
+    perturbations.append(perturbation)
+
+    perturbed_u0 = run_forward(initial_condition=perturbation, annotate=False, dump=False)
+    functional_values.append(assemble(perturbed_u0*perturbed_u0*dx))
+
+  # First-order Taylor remainders (not using adjoint)
+  no_gradient = [abs(perturbed_f - f_direct) for perturbed_f in functional_values]
+  no_gradient_conv = convergence_order(no_gradient)
+
+  print "Taylor remainder without adjoint information: ", no_gradient
+  print "Convergence orders for Taylor remainder without adjoint information (should all be 1): ", no_gradient_conv
+
+  adjoint_vector = numpy.array(final_adjoint.vector())
+
+  with_gradient = []
+  for i in range(len(perturbations)):
+    remainder = abs(functional_values[i] - f_direct - numpy.dot(adjoint_vector, numpy.array(perturbations[i].vector())))
+    with_gradient.append(remainder)
+
+  print "Taylor remainder with adjoint information: ", with_gradient
+  print "Convergence orders for Taylor remainder wth adjoint information (should all be 2): ", convergence_order(with_gradient)
+
+test_ic_gradient(final_adjoint)
