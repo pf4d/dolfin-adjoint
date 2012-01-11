21ef787b012847ee169ccf4cb95f1ec8d7ef3401
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-01-11 16:37:41 +0000

    Make it so that the main program for the sw model is called sw.py.

--- tests/sw/kelvin_new.py
+++ tests/sw/kelvin_new.py
@@ -1,9 +1,9 @@
 from dolfin import Mesh, Expression
 from math import exp, sqrt, pi
 
-import sw
+import sw_lib
 
-params=sw.parameters({
+params=sw_lib.parameters({
     'depth' : 5.,
     'g' : 10.,
     'f' : 1.0313e-4,
