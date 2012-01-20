4ece322a678675a97c544f5b13bbd3be3c87d49a
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-01-20 13:31:26 +0000

    A bit of code reorganisation. Put the CoeffStore into its own file. More to come.

---
+++ coeffstore.py
@@ -0,0 +1,37 @@
+import libadjoint
+
+class CoeffStore(object):
+  '''This object manages the mapping from Dolfin coefficients to libadjoint Variables.
+  In the process, it also manages the incrementing of the timestep associated with each
+  variable, so that the user does not have to manually manage the time information.'''
+  def __init__(self):
+    self.coeffs={}
+    self.libadjoint_timestep = -1
+
+  def next(self, coeff):
+    '''Increment the timestep corresponding to the provided Dolfin
+    coefficient and then return the corresponding libadjoint variable.'''
+
+    try:
+      (timestep, iteration) = self.coeffs[coeff]
+      if timestep == max(self.libadjoint_timestep, 0):
+        self.coeffs[coeff] = (timestep, iteration + 1)
+      else:
+        self.coeffs[coeff] = (self.libadjoint_timestep, 0)
+    except KeyError:
+      self.coeffs[coeff] = (max(self.libadjoint_timestep, 0), 0)
+
+    (timestep, iteration) = self.coeffs[coeff]
+    return libadjoint.Variable(str(coeff), timestep, iteration)
+
+  def __getitem__(self, coeff):
+    '''Return the libadjoint variable corresponding to coeff.'''
+
+    if not self.coeffs.has_key(coeff):
+      self.coeffs[coeff] = (max(self.libadjoint_timestep, 0), 0)
+
+    (timestep, iteration) = self.coeffs[coeff]
+    return libadjoint.Variable(str(coeff), timestep, iteration)
+
+  def increment_timestep(self):
+    self.libadjoint_timestep += 1
--- solving.py
+++ solving.py
@@ -14,51 +14,15 @@ import time
 
 import assembly
 import expressions
+import coeffstore
 
-debugging={}
-
-class CoeffStore(object):
-  '''This object manages the mapping from Dolfin coefficients to libadjoint Variables.
-  In the process, it also manages the incrementing of the timestep associated with each
-  variable, so that the user does not have to manually manage the time information.'''
-  def __init__(self):
-    self.coeffs={}
-    self.libadjoint_timestep = -1
-
-  def next(self, coeff):
-    '''Increment the timestep corresponding to the provided Dolfin
-    coefficient and then return the corresponding libadjoint variable.'''
-
-    try:
-      (timestep, iteration) = self.coeffs[coeff]
-      if timestep == max(self.libadjoint_timestep, 0):
-        self.coeffs[coeff] = (timestep, iteration + 1)
-      else:
-        self.coeffs[coeff] = (self.libadjoint_timestep, 0)
-    except KeyError:
-      self.coeffs[coeff] = (max(self.libadjoint_timestep, 0), 0)
-
-    (timestep, iteration) = self.coeffs[coeff]
-    return libadjoint.Variable(str(coeff), timestep, iteration)
-
-  def __getitem__(self, coeff):
-    '''Return the libadjoint variable corresponding to coeff.'''
-
-    if not self.coeffs.has_key(coeff):
-      self.coeffs[coeff] = (max(self.libadjoint_timestep, 0), 0)
-
-    (timestep, iteration) = self.coeffs[coeff]
-    return libadjoint.Variable(str(coeff), timestep, iteration)
-
-  def increment_timestep(self):
-    self.libadjoint_timestep += 1
-
-adj_variables=CoeffStore()
+adj_variables = coeffstore.CoeffStore()
 def adj_inc_timestep():
   adj_variables.increment_timestep()
 
 # Set record_all to true to enable recording all variables in the forward
 # run. This is primarily useful for debugging.
+debugging={}
 debugging["record_all"] = False
 debugging["test_hermitian"] = None
 debugging["test_derivative"] = None
