bd4fd41c00f4feddb9a23e13d554c7fb99f64845
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-01-27 22:26:09 +0000

    Simplify the matrix_free_simple test significantly.

--- tests/matrix_free_simple/matrix_free_simple.py
+++ tests/matrix_free_simple/matrix_free_simple.py
@@ -5,42 +5,20 @@ import sys
 
 debugging["record_all"] = True
 
-mesh = UnitSquare(32, 32)
+mesh = UnitSquare(4, 4)
 V = FunctionSpace(mesh, 'CG', 1)
 bc = DirichletBC(V, Constant(0.0), lambda x, on_boundary: on_boundary)
 u = TrialFunction(V); v = TestFunction(V);
 
 def main(ic):
-  A = assemble(inner(grad(u), grad(v))*dx)
-  bc.apply(A)
   b = assemble(ic * v* dx)
   bc.apply(b)
 
-  class KrylovMatrix(PETScKrylovMatrix) :
-      def __init__(self) :
-          PETScKrylovMatrix.__init__(self, V.dim(), V.dim())
-
-      def mult(self, *args):
-          y = PETScVector(V.dim())
-          A.mult(args[0], y)
-          args[1].set_local(y.array())
-
-      def transpmult(self, *args):
-          y = PETScVector(V.dim())
-          A.transpmult(args[0], y)
-          args[1].set_local(y.array())
-
-      def dependencies(self):
-        return []
-
-  #y = Function(V)
-  #solve(A, y.vector(), b, "cg", "none")
-
   x = Function(V)
   KrylovSolver = AdjointPETScKrylovSolver("cg","none")
-  KrylovSolver.solve(KrylovMatrix(), down_cast(x.vector()), down_cast(b))
+  mat = AdjointKrylovMatrix(inner(grad(u), grad(v))*dx, bcs=bc)
+  KrylovSolver.solve(mat, down_cast(x.vector()), down_cast(b))
 
-  #assert (x.vector() - y.vector()).norm("l2") == 0
   return x
 
 if __name__ == "__main__":
