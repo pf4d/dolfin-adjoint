e79885a6fe9110abe385c5837141403e698080d2
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-03-07 13:06:58 +0000

    We can successfully compute that the biggest singular value of the identity matrix is 1. At least *something* works ..

--- tests/svd_simple/svd_simple.py
+++ tests/svd_simple/svd_simple.py
@@ -5,19 +5,17 @@ from dolfin_adjoint import *
 import sys
 
 mesh = UnitSquare(4, 4)
-V = FunctionSpace(mesh, "CG", 3)
+V = FunctionSpace(mesh, "CG", 1)
 debugging["record_all"] = True
 
 def main(ic, annotate=False):
   u = TrialFunction(V)
   v = TestFunction(V)
 
-  bc = DirichletBC(V, "-1.0", "on_boundary")
-
   mass = inner(u, v)*dx
   soln = Function(V)
 
-  solve(mass == action(mass, ic), soln, bc, annotate=annotate)
+  solve(mass == action(mass, ic), soln, annotate=annotate)
   return soln
 
 if __name__ == "__main__":
@@ -26,5 +24,8 @@ if __name__ == "__main__":
   soln = main(ic, annotate=True)
 
   svd = adj_compute_tlm_svd(ic, soln, 1)
-  print "Maximal singular value: ", svd.get_svd(0, return_error=True)
+  (sigma, error) = svd.get_svd(0, return_error=True)
+
+  if (abs(sigma - 1.0) > 1.0e-15):
+    sys.exit(1)
 
