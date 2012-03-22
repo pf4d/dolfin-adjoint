508ddc6a26a766254d29838d0c2c9bd567a2de48
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-03-22 12:04:21 +0000

    Add a setup.py. Make the directory structure slightly less insane.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup (name = 'dolfin_adjoint',
+       version = '0.6',
+       description = 'Automatically derive the discrete adjoint of DOLFIN models',
+       author = 'The dolfin_adjoint team',
+       author_email = 'patrick.farrell06@imperial.ac.uk',
+       packages = ['dolfin_adjoint'],
+       package_dir = {'dolfin_adjoint': 'dolfin_adjoint'})
--- tests/test.py
+++ tests/test.py
@@ -10,7 +10,7 @@ test_cmds = {'tlm_simple': 'mpirun -n 2 python tlm_simple.py',
 basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
 subdirs = [x for x in os.listdir(basedir) if os.path.isdir(os.path.join(basedir, x))]
 
-os.putenv('PYTHONPATH', os.path.abspath(os.path.join(basedir, os.path.pardir, os.path.pardir)))
+os.putenv('PYTHONPATH', os.path.abspath(os.path.join(basedir, os.path.pardir)))
 
 for subdir in sorted(subdirs):
   test_cmd = test_cmds.get(subdir, 'python %s.py' % subdir)
