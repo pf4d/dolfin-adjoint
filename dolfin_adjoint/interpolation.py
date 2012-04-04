db9a526f1ac56ea11288dc24af0819813a294d23
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-04-04 12:42:25 +0100

    Don't try to checkpoint Expressions, it tends not to work.

--- dolfin_adjoint/interpolation.py
+++ dolfin_adjoint/interpolation.py
@@ -6,38 +6,39 @@ import ufl
 def interpolate(v, V, annotate=True):
 
   out = dolfin.interpolate(v, V)
-  rhsdep = solving.adj_variables[v]
 
-  if isinstance(v, dolfin.Function) and annotate and solving.adjointer.variable_known(rhsdep):
-    block_name = "Identity: %s" % str(V)
-    if len(block_name) > int(libadjoint.constants.adj_constants["ADJ_NAME_LEN"]):
-      block_name = block_name[0:int(libadjoint.constants.adj_constants["ADJ_NAME_LEN"])-1]
-    identity_block = libadjoint.Block(block_name)
+  if isinstance(v, dolfin.Function) and annotate:
+    rhsdep = solving.adj_variables[v]
+    if solving.adjointer.variable_known(rhsdep):
+      block_name = "Identity: %s" % str(V)
+      if len(block_name) > int(libadjoint.constants.adj_constants["ADJ_NAME_LEN"]):
+        block_name = block_name[0:int(libadjoint.constants.adj_constants["ADJ_NAME_LEN"])-1]
+      identity_block = libadjoint.Block(block_name)
 
-    def identity_assembly_cb(variables, dependencies, hermitian, coefficient, context):
-      assert coefficient == 1
-      return (solving.Matrix(ufl.Identity(V.dim())), solving.Vector(dolfin.Function(V)))
+      def identity_assembly_cb(variables, dependencies, hermitian, coefficient, context):
+        assert coefficient == 1
+        return (solving.Matrix(ufl.Identity(V.dim())), solving.Vector(dolfin.Function(V)))
 
-    identity_block.assemble = identity_assembly_cb
+      identity_block.assemble = identity_assembly_cb
 
-    rhs = InterpolateRHS(v, V)
+      rhs = InterpolateRHS(v, V)
 
-    no_registered = solving.register_initial_conditions(zip(rhs.coefficients(),rhs.dependencies()), linear=True)
+      no_registered = solving.register_initial_conditions(zip(rhs.coefficients(),rhs.dependencies()), linear=True)
 
-    if solving.adjointer.first_solve:
-      solving.adjointer.first_solve = False
-      if no_registered > 0:
-        solving.adj_inc_timestep()
+      if solving.adjointer.first_solve:
+        solving.adjointer.first_solve = False
+        if no_registered > 0:
+          solving.adj_inc_timestep()
 
-    dep = solving.adj_variables.next(out)
+      dep = solving.adj_variables.next(out)
 
-    if solving.debugging["record_all"]:
-      solving.adjointer.record_variable(dep, libadjoint.MemoryStorage(solving.Vector(out)))
+      if solving.debugging["record_all"]:
+        solving.adjointer.record_variable(dep, libadjoint.MemoryStorage(solving.Vector(out)))
 
-    initial_eq = libadjoint.Equation(dep, blocks=[identity_block], targets=[dep], rhs=rhs)
-    cs = solving.adjointer.register_equation(initial_eq)
+      initial_eq = libadjoint.Equation(dep, blocks=[identity_block], targets=[dep], rhs=rhs)
+      cs = solving.adjointer.register_equation(initial_eq)
 
-    solving.do_checkpoint(cs, dep)
+      solving.do_checkpoint(cs, dep)
 
   return out
 
