fca9d04ca0d48ca0a068c7f63f271f7e03736fcc
Simon Funke <simon.funke@gmail.com>
2012-06-11 23:45:15 +0100

    First steps towards supporting parallel optimisation: This commit adds a function that serialises a local array, i.e. each processor sends the local part of the array to everyone and receives all array parts from the other processors. The return value is a global array that contains all values and is equivalent on each processor. This functionality relies on mpi4py, hence introduces an additional external dependency to dolfin-adjoint.

--- dolfin_adjoint/optimisation.py
+++ dolfin_adjoint/optimisation.py
@@ -1,6 +1,30 @@
 from dolfin import *
 from dolfin_adjoint import * 
+from mpi4py import MPI
 import numpy
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def serialise_array(comm, local_array):
+    ''' Uses MPI to serialise an distributed array. The argument local_array must 
+        be an array containg the local contribution of the array. The return value 
+        will be the global array. '''
+
+    # First, use a Allgather to collect the required information about the array size on each processor
+    p = comm.Get_size()
+    sendcount = numpy.zeros(p, dtype='i')
+    lsendcount = numpy.array([len(local_array)], dtype='i')
+    comm.Allgather([lsendcount,  MPI.INT],
+                   [sendcount, MPI.INT])
+    count = sum(sendcount) 
+    displs = [sum(sendcount[:i]) for i in range(len(sendcount))]
+
+    # Second use a Allgatherv to distribute the array to all processors
+    array = numpy.zeros(count, dtype='d')
+    comm.Allgatherv([local_array, MPI.DOUBLE],
+                    [array, (sendcount, displs),  MPI.DOUBLE])
+    return array
 
 def generate_array_bounds(bounds, m):
     
@@ -104,7 +128,8 @@ def minimise(reduced_functional, functional, parameter, m, algorithm, **kwargs):
         dJdm = utils.compute_gradient(functional, parameter)
 
         if dolfin.parameters["optimisation"]["test_gradient"]:
-            minconv = utils.test_gradient_array(reduced_functional_array, to_array(dJdm), m_array, seed = dolfin.parameters["optimisation"]["test_gradient_seed"])
+            minconv = utils.test_gradient_array(reduced_functional_array, to_array(dJdm), m_array, 
+                                                seed = dolfin.parameters["optimisation"]["test_gradient_seed"])
             if minconv < 1.9:
                 raise RuntimeWarning, "A gradient test failed during execution."
             else:
