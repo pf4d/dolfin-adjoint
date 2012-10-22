c604a2fc23be0f3695053bd6feeb456a3e9d1950
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-10-22 10:43:53 +0100

    Make that new function public.

--- dolfin_adjoint/ui.py
+++ dolfin_adjoint/ui.py
@@ -4,7 +4,7 @@ from parameter import InitialConditionParameter, ScalarParameter, ScalarParamete
 from lusolver import LUSolver
 #from matrix_free import down_cast, AdjointPETScKrylovSolver, AdjointKrylovMatrix
 from solving import solve, adj_html, adj_reset, adj_checkpointing
-from adjglobals import adj_inc_timestep, adjointer, adj_check_checkpoints
+from adjglobals import adj_start_timestep, adj_inc_timestep, adjointer, adj_check_checkpoints
 from gst import compute_gst, compute_propagator_matrix, perturbed_replay
 from utils import replay_dolfin, convergence_order, compute_adjoint, compute_tlm, compute_gradient, estimate_error
 from utils import test_initial_condition_adjoint, test_initial_condition_adjoint_cdiff, test_initial_condition_tlm, test_scalar_parameter_adjoint, test_scalar_parameters_adjoint, taylor_test
