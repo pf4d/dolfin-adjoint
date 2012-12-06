d7a149ef47333df5ceabb6f8374c294444d11e9f
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-12-06 18:51:59 +0000

    Add a solver_type argument to fix the projection.

--- tests/projection/projection.py
+++ tests/projection/projection.py
@@ -11,7 +11,7 @@ dolfin.parameters["adjoint"]["record_all"] = True
 
 def main(ic, annotate=False):
   bc = DirichletBC(V2, "-1.0", "on_boundary")
-  soln = project(ic, V2, bcs=bc, annotate=annotate)
+  soln = project(ic, V2, bcs=bc, solver_type='lu', annotate=annotate)
   return soln
 
 if __name__ == "__main__":
