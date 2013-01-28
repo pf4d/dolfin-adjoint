8eb8f6b8276f0288f8718e4830e118bfeb6ce31e
Simon Funke <simon.funke@gmail.com>
2013-01-28 14:17:11 +0000

    Implement the function replace_tape_ic_value which can be used to replace the initial condition values on the libadjoit's tape. This is heavily used in the optimisation framework to update the optimisation control parameter, but is also usefull for other things.

--- dolfin_adjoint/reduced_functional.py
+++ dolfin_adjoint/reduced_functional.py
@@ -66,8 +66,36 @@ def set_local(m_list, m_global_array):
         else:
             raise TypeError, 'Unknown parameter type'
 
-class DummyEquation(object):
-    pass
+
+global_eqn_list = {}
+def replace_tape_ic_value(variable, new_value):
+    ''' Replaces the initial condition value of the given variable by registering a new equation of the rhs. '''
+    class DummyEquation(object):
+        pass
+
+    if hasattr(new_value, 'vector'):
+        # ... since these are duplicated and then occur as rhs in the annotation. 
+        # Therefore, we need to update the right hand side callbacks for
+        # the equation that targets the associated variable.
+
+        # Create a RHS object with the new control values
+        init_rhs = adjlinalg.Vector(new_value).duplicate()
+        init_rhs.axpy(1.0, adjlinalg.Vector(new_value))
+        rhs = adjrhs.RHS(init_rhs)
+        # Register the new rhs in the annotation
+        eqn = DummyEquation() 
+        eqn_nb = variable.var.equation_nb(adjointer)
+        eqn.equation = adjointer.adjointer.equations[eqn_nb]
+        rhs.register(eqn)
+        # Store the equation as a class variable in order to keep a python reference in the memory
+        global_eqn_list[variable.var.equation_nb] = eqn
+    elif hasattr(new_value, "value_size"): 
+        # Constants are not duplicated in the annotation. That is, changing a constant that occurs
+        # in the forward model will also change the forward replay with libadjoint.
+        # However, this is not the case for functions...
+        pass
+    else:
+        raise NotImplementedError, "Can only replace a dolfin.Functions or dolfin.Constants"
 
 class ReducedFunctional(object):
     ''' This class implements the reduced functional for a given functional/parameter combination. The core idea 
@@ -109,29 +137,7 @@ class ReducedFunctional(object):
 
         # Update the parameter values
         for i in range(len(value)):
-            if hasattr(value[i], 'vector'):
-                # ... since these are duplicated and then occur as rhs in the annotation. 
-                # Therefore, we need to update the right hand side callbacks for
-                # the equation that targets the associated variable.
-
-                # Create a RHS object with the new control values
-                init_rhs = adjlinalg.Vector(value[i]).duplicate()
-                init_rhs.axpy(1.0, adjlinalg.Vector(value[i]))
-                rhs = adjrhs.RHS(init_rhs)
-                # Register the new rhs in the annotation
-                eqn = DummyEquation() 
-                eqn_nb = self.parameter[i].var.equation_nb(adjointer)
-                eqn.equation = adjointer.adjointer.equations[eqn_nb]
-                # Store the equation as a class variable in order to keep a python reference in the memory
-                self.eqns.append(eqn)
-                rhs.register(self.eqns[-1])
-            elif hasattr(value[i], "value_size"): 
-                # Constants are not duplicated in the annotation. That is, changing a constant that occurs
-                # in the forward model will also change the forward replay with libadjoint.
-                # However, this is not the case for functions...
-                pass
-            else:
-                raise NotImplementedError, "The ReducedFunctional class currently only works for parameters that are Functions"
+            replace_tape_ic_value(self.parameter[i], value[i])
 
         # Replay the annotation and evaluate the functional
         func_value = 0.
