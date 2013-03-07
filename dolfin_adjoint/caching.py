375d81d32d46c2d0d6562c0d8548152ab49a9bc2
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2013-03-06 19:02:47 -0500

    In the caching, exploit the fact that the SOA and ADM operators are the same.

--- dolfin_adjoint/caching.py
+++ dolfin_adjoint/caching.py
@@ -1,16 +1,32 @@
+import re
+
+soa_to_adj = re.compile(r'\[(?P<func>Functional:.*?):Solution:.*\]')
 
 # For caching strategies: a dictionary that maps adj_variable to LUSolver
 # Not used by default
+
+def canonicalise(var):
+  # Return a string representation of var for indexing into the LU cache.
+
+  s = str(var)
+
+  # Since the SOA operator is always the same as the ADM, we can replace all
+  # requests for SOA operators with ADM ones
+  if var.type == 'ADJ_SOA':
+    s = soa_to_adj.sub(r'[\1]', s).replace("SecondOrderAdjoint", "Adjoint")
+
+  return s
+
 class LUCache(dict):
   def __getitem__(self, x):
-    return dict.__getitem__(self, str(x))
+    return dict.__getitem__(self, canonicalise(x))
 
   def __setitem__(self, x, y):
-    return dict.__setitem__(self, str(x), y)
+    return dict.__setitem__(self, canonicalise(x), y)
 
   def __delitem__(self, x):
-    return dict.__delitem__(self, str(x))
+    return dict.__delitem__(self, canonicalise(x))
 
   def __contains__(self, x):
-    return dict.__contains__(self, str(x))
+    return dict.__contains__(self, canonicalise(x))
 
