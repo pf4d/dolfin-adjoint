da6e71b05fe9b2584a7b6e8df9e917109cd91de1
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2013-05-31 15:17:15 +0100

    Add a compute_gradient_tlm that returns an object that computes each gradient-vector product using the TLM, instead of computing it all at once using the adjoint.

---
+++ tests/tlm_burgers/tlm_burgers.py
@@ -0,0 +1,57 @@
+"""
+Implementation of Burger's equation with nonlinear solve in each
+timestep
+"""
+
+import sys
+
+from dolfin import *
+from dolfin_adjoint import *
+
+n = 30
+mesh = UnitIntervalMesh(n)
+V = FunctionSpace(mesh, "CG", 2)
+
+def Dt(u, u_, timestep):
+    return (u - u_)/timestep
+
+def main(ic, annotate=False):
+
+    u_ = Function(ic, name="Velocity")
+    u = Function(V, name="VelocityNext")
+    v = TestFunction(V)
+
+    nu = Constant(0.0001)
+
+    timestep = Constant(1.0/n)
+
+    F = (Dt(u, u_, timestep)*v
+         + u*u.dx(0)*v + nu*u.dx(0)*v.dx(0))*dx
+    bc = DirichletBC(V, 0.0, "on_boundary")
+
+    t = 0.0
+    end = 0.2
+    while (t <= end):
+        solve(F == 0, u, bc, annotate=annotate)
+        u_.assign(u, annotate=annotate)
+
+        t += float(timestep)
+        adj_inc_timestep()
+
+    return u_
+
+if __name__ == "__main__":
+
+    ic = project(Expression("sin(2*pi*x[0])"),  V)
+    forward = main(ic, annotate=True)
+
+    J = Functional(forward*forward*dx*dt[FINISH_TIME])
+    Jic = assemble(forward*forward*dx)
+    dJdic = compute_gradient_tlm(J, InitialConditionParameter("Velocity"), forget=False)
+
+    def Jfunc(ic):
+      forward = main(ic, annotate=False)
+      return assemble(forward*forward*dx)
+
+    minconv = taylor_test(Jfunc, InitialConditionParameter("Velocity"), Jic, dJdic, seed=1.0e-3, perturbation_direction=interpolate(Expression("cos(x[0])"), V))
+    assert minconv > 1.7
