60af3a37977e48e6c7c5653542a3011129bc240b
Simon Funke <simon.funke@gmail.com>
2013-07-26 21:07:19 +0100

    add a missing file

---
+++ dolfin_adjoint/optimization/__init__.py
@@ -0,0 +1,10 @@
+"""
+The optimization module is automatically imported by dolfin-adjoint 
+"""
+
+__version__ = '1.1.0+'
+__author__  = 'Patrick Farrell and Simon Funke'
+__credits__ = ['Patrick Farrell', 'Simon Funke', 'David Ham', 'Marie Rognes']
+__license__ = 'LGPL-3'
+__maintainer__ = 'Simon Funke'
+__email__ = 's.funke09@imperial.ac.uk'
