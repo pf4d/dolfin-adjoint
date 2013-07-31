b74be1bbd854ae3fe29e4b7c8dfac58c4d06dd21
Simon Funke <simon.funke@gmail.com>
2013-07-31 16:57:37 +0100

    Pass all parameters to the fortran implementation of the strong wolfe line search and add an option to ignore line search warnings such as stp=stpmax (ignore on is default now)

--- dolfin_adjoint/optimization/line_search/strong_wolfe.py
+++ dolfin_adjoint/optimization/line_search/strong_wolfe.py
@@ -4,7 +4,7 @@ from numpy import zeros
 from dolfin import info_red
 
 class StrongWolfeLineSearch(LineSearch):
-    def __init__(self, ftol = 1e-4, gtol = 0.9, xtol = 1e-1, start_stp = 1.0, stpmin = None, stpmax = None, verify = False):
+    def __init__(self, ftol = 1e-4, gtol = 0.9, xtol = 1e-1, start_stp = 1.0, stpmin = None, stpmax = None, verify = False, ignore_warnings = True):
         '''
         This class implements a line search algorithm whose steps 
         satisfy the strong Wolfe conditions (i.e. they satisfies a 
@@ -29,15 +29,17 @@ class StrongWolfeLineSearch(LineSearch):
 
         The function arguments are:
 
-           ftol      | a nonnegative tolerance for the sufficient decrease condition.
-           gtol      | a nonnegative tolerance for the curvature condition.
-           xtol      | a nonnegative relative tolerance for an acceptable step.
-           start_stp | a guess for an initial step size. 
-           stpmin    | a nonnegative lower bound for the step.
-           stpmax    | a nonnegative upper bound for the step.
-           verify    | if True, the step is compared to the Fortran implementation 
-                       (note: this assumes that the Python module dcsrch_fortran is compiled
-                        and in the PYTHONPATH)
+           ftol           | a nonnegative tolerance for the sufficient decrease condition.
+           gtol           | a nonnegative tolerance for the curvature condition.
+           xtol           | a nonnegative relative tolerance for an acceptable step.
+           start_stp      | a guess for an initial step size. 
+           stpmin         | a nonnegative lower bound for the step.
+           stpmax         | a nonnegative upper bound for the step.
+           verify         | if True, the step is compared to the Fortran implementation 
+                          | (note: this assumes that the Python module dcsrch_fortran is compiled
+                          |  and in the PYTHONPATH)
+          ignore_warnings | Continue if the line search ends with a warnings (e.g. stp = stpmax). 
+                          | Default: True
 
         References:
          Mor'e, J.J., and Thuente, D.J., 1992, Line search algorithms with guaranteed 
@@ -46,13 +48,14 @@ class StrongWolfeLineSearch(LineSearch):
               from MINPACK-2, 1993, Argonne National Laboratory and University of Minnesota.
         '''
 
-        self.ftol       = ftol 
-        self.gtol       = gtol
-        self.xtol       = xtol
-        self.start_stp  = start_stp
-        self.stpmin     = stpmin
-        self.stpmax     = stpmax
-        self.verify     = verify
+        self.ftol            = ftol 
+        self.gtol            = gtol
+        self.xtol            = xtol
+        self.start_stp       = start_stp
+        self.stpmin          = stpmin
+        self.stpmax          = stpmax
+        self.verify          = verify
+        self.ignore_warnings = ignore_warnings
 
     def search(self, phi, phi_dphi):
         ''' Performs the line search on the function phi. 
@@ -73,6 +76,8 @@ class StrongWolfeLineSearch(LineSearch):
         f, g = phi_dphi(0)
 
         # Compute an estimate for the maximum step size 
+        if not self.stpmin:
+            self.stpmin = 0.
         if not self.stpmax:
             self.stpmax = max(4*min(self.start_stp, 1.0), 0.1*f/(-g*self.ftol))
 
@@ -86,10 +91,10 @@ class StrongWolfeLineSearch(LineSearch):
 
         if "Error" in task:
             raise RuntimeError, task
-        elif "Warning" in task:
+        elif "Warning" in task and not self.ignore_warnings:
             raise Warning, task
         else:
-            assert task=="Convergence"
+            assert task=="Convergence" or ("Warning" in task and self.ignore_warnings)
 
             if self.verify:
                 # Recompute the step with the Fortran implementation and compare
@@ -112,9 +117,10 @@ class StrongWolfeLineSearch(LineSearch):
 
         try: 
             import pyswolfe
-
             dphi = lambda x: phi_dphi(x)[1]
-            ls_fort = pyswolfe.StrongWolfeLineSearch(phi(0), dphi(0), 1.0, phi, dphi, gtol=self.gtol)
+            ls_fort = pyswolfe.StrongWolfeLineSearch(phi(0), dphi(0), 1.0, phi, dphi, gtol=self.gtol, 
+                                                     xtol=self.xtol, ftol=self.ftol, stp=self.start_stp, 
+                                                     stpmin=self.stpmin, stpmax=self.stpmax)
             ls_fort.search()
             return ls_fort.stp 
 
