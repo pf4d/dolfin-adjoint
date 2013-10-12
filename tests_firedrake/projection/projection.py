c802877dce2d514362f0bb5a406531409f665436
Simon Funke <simon.funke@gmail.com>
2013-10-12 18:42:36 +0100

    Add a Taylor test for the projection in firedrake.

---
+++ tests_firedrake/projection/projection.py
@@ -0,0 +1,35 @@
+import random
+
+from firedrake import *
+from firedrake_adjoint import *
+import sys
+
+mesh = UnitSquareMesh(4, 4)
+V3 = FunctionSpace(mesh, "CG", 3)
+V2 = FunctionSpace(mesh, "CG", 2)
+firedrake.parameters["adjoint"]["record_all"] = True
+
+def main(ic, annotate=False):
+  soln = project(ic, V2, annotate=annotate)
+  return soln
+
+if __name__ == "__main__":
+
+  ic = project(Expression("x[0]*(x[0]-1)*x[1]*(x[1]-1)"), V3)
+  soln = main(ic, annotate=True)
+
+  adj_html("projection_forward.html", "forward")
+  replay_dolfin()
+
+  J = Functional(soln*soln*dx*dt[FINISH_TIME])
+  Jic = assemble(soln*soln*dx)
+  dJdic = compute_gradient(J, InitialConditionParameter(ic), forget=False)
+
+  def J(ic):
+    soln = main(ic, annotate=False)
+    return assemble(soln*soln*dx)
+
+  minconv = taylor_test(J, InitialConditionParameter(ic), Jic, dJdic)
+  if minconv < 1.9:
+    sys.exit(1)
+
