073de382e34c41bcf71f32107da740e80c62debb
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2013-10-14 16:25:38 +0100

    Remove the reference to the unused file

--- dolfin_adjoint/ui.py
+++ dolfin_adjoint/ui.py
@@ -16,7 +16,6 @@ from projection import project
 from function import Function
 from interpolation import interpolate
 from constant import Constant
-from unimplemented import *
 from timeforms import dt, TimeMeasure, START_TIME, FINISH_TIME
 from reduced_functional import ReducedFunctional
 from reduced_functional_numpy import ReducedFunctionalNumPy
