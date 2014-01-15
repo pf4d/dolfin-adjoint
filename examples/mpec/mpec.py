82db2d30cab5f4e443434c0267b6a29d41c2b068
Simon Funke <simon.funke@gmail.com>
2014-01-15 10:22:16 +0100

    Improve and comment the MPEC example code.
    One thing does not work yet:
    The state solution after an optimisation run should be used as an initial guess for the optimisation.

--- examples/mpec/mpec.py
+++ examples/mpec/mpec.py
@@ -17,56 +17,81 @@ author={Hinterm{\"u}ller, M. and Kopacka, I.},
 pages={111-145},
 }
 
+The problem is to solve following Mathematical Problem with Equilibrium Constraints (MPEC):
+
+  min 1/2 ||y-y_d||^2 + nu/2 ||u||^2
+
+subject to the variational equality (VI)
+
+  (\nabla y, \nabla(v - y)) >= (f+u, v-y) for all v >= 0,
+                          y >= 0,
+
+and bounds on the control functional
+
+a <= u <= b,
+
+where u is the control function, y is the solution of the variational inequality, y_d is solution to be matched, f is a prescribed source term, \nu is a regularisation parameter and a, b are functions defining the upper and lower bound for the control.
+
+The solution approach taken here is to approximate the variational inequality by a sequence of partial differential equation with the penalisation technique, 
+and to solve the resulting sequence of PDE-constrained optimisation problems.
+
 """
+
 
 from dolfin import *
 from dolfin_adjoint import *
+#set_log_level(ERROR)
 
-n = 128
-mesh = UnitSquareMesh(n, n)
+# Define a smooth approximation of the (pointwise) maximum operator
+def smoothmax(r, eps=1e-4):
+  return conditional(gt(r, eps), r - eps/2, conditional(lt(r, 0), 0, r**2 / (2*eps))) 
 
-V = FunctionSpace(mesh, "CG", 1)
-gamma = Constant(1e3) # 1.0 / alpha in the paper
-
+mesh = UnitSquareMesh(128, 128)
+V = FunctionSpace(mesh, "CG", 1)  # The function space for the solution and control functions
 y = Function(V, name="Solution")
 u = Function(V, name="Control")
 w = TestFunction(V)
 
-gamma = Constant(1e3) # 1.0 / alpha in the paper
-eps = 1e-4
-
-def smoothmax(r):
-  return conditional(gt(r, eps), r - eps/2, conditional(lt(r, 0), 0, r**2 / (2*eps))) 
-def uflmax(a, b):
-  return conditional(gt(a, b), a, b) 
-
-f = interpolate(Expression("-std::abs(x[0]*x[1] - 0.5) + 0.25"), V)
-F = inner(grad(y), grad(w))*dx - gamma * inner(smoothmax(-y), w)*dx - inner(f + u, w)*dx
-
+# Define the approximated variational inequality and solve the resulting PDE
+alpha = Constant(1e-3)  # The penalisation parameter
+f = interpolate(Expression("-std::abs(x[0]*x[1] - 0.5) + 0.25"), V)  # The source term
+F = inner(grad(y), grad(w))*dx - 1 / alpha * inner(smoothmax(-y), w)*dx - inner(f + u, w)*dx  
 bc = DirichletBC(V, 0.0, "on_boundary")
-solve(F == 0, y, bcs = bc, solver_parameters={"newton_solver": {"maximum_iterations": 30}})
+solve(F == 0, y, bcs=bc)
 
+# Define the functional of interest
 yd = Function(f)
-alpha = 1e-2
-J = Functional(0.5*inner(y - yd, y - yd)*dx + alpha/2*inner(u, u)*dx)
+nu = 0.01
+J = Functional(0.5*inner(y - yd, y - yd)*dx + nu/2*inner(u, u)*dx)
 
+# Formulate the reduced problem
 m = SteadyParameter(u)
 Jhat = ReducedFunctional(J, m)
 
 ypvd = File("output/y_opt.pvd") 
 upvd = File("output/u_opt.pvd") 
-for g in [1000*2**i for i in range(12)]:
-  print "Set gamma = ", g
-  gamma.assign(g)
-  u_opt = minimize(Jhat, bounds = (0.01, 0.03), options = {"disp": True, "gtol": 1e-12, "ftol": 1e-100})
-  Jhat(u_opt)
-  y_opt = DolfinAdjointVariable(y, iteration=-1).tape_value()
+
+Jalpha = ReducedFunctional(J, ScalarParameter(alpha))
+
+# Solve the MPECs as a sequence of PDE-constrained optimisation problems 
+for i in range(12):
+  # Update the penalisation value
+  alpha.assign(float(alpha)/2)
+  info_green("Set alpha to %f." % float(alpha))
+  Jalpha(alpha)
+
+  # Solve the optimisation problem
+  u_opt = minimize(Jhat, bounds=(0.01, 0.03), options={"disp": True, "gtol": 1e-12, "ftol": 1e-100})
+  
+  # Use the optimised state solution as an initial guess 
+  # for the Newton solver in the next optimisation round
+  y_opt = DolfinAdjointVariable(y).tape_value()
+  reduced_functional.replace_tape_ic_value(InitialConditionParameter(y), y_opt)
+
+  # Save the result and print some statistics
   ypvd << y_opt
   upvd << u_opt
-
-  feasibility = sqrt(assemble(inner((uflmax(0, -y_opt)), (uflmax(0, -y_opt)))*dx))
-  info_red("Feasibility: %s"%  feasibility)
-  info_red("Norm of y: %s"% sqrt(assemble(inner(y_opt, y_opt)*dx)))
-  info_red("Norm of u_opt: %s"% sqrt(assemble(inner(u_opt, u_opt)*dx)))
-
-  reduced_functional.replace_tape_ic_value(m, y_opt)
+  feasibility = sqrt(assemble(inner((Max(0, -y_opt)), (Max(0, -y_opt)))*dx))
+  info_green("Feasibility: %s" % feasibility)
+  info_green("Norm of y: %s" % sqrt(assemble(inner(y_opt, y_opt)*dx)))
+  info_green("Norm of u_opt: %s" % sqrt(assemble(inner(u_opt, u_opt)*dx)))
