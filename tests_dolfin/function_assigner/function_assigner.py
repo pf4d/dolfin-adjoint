b10f78095bb63e19fc406334917f29aa43b0cff7
Patrick Farrell <patrick.farrell@imperial.ac.uk>
2014-03-12 04:17:04 +0000

    Make the test a bit more complicated, still works.

--- tests_dolfin/function_assigner/function_assigner.py
+++ tests_dolfin/function_assigner/function_assigner.py
@@ -11,33 +11,27 @@ V = VectorFunctionSpace(mesh, "CG", 2)
 P = FunctionSpace(mesh, "CG", 1)
 Z = MixedFunctionSpace([V, P])
 
-def main(p):
+def main(u, p):
+  assigner_u = FunctionAssigner(Z.sub(0), V)
   assigner_p = FunctionAssigner(Z.sub(1), P)
 
   z = Function(Z, name="Output")
 
+  assigner_u.assign(z.sub(0), u)
   assigner_p.assign(z.sub(1), p)
 
   return z
 
 if __name__ == "__main__":
-  #p = interpolate(Constant(1), P, name="Pressure")
+  u = interpolate(Constant((1,)), V, name="Velocity")
   p = interpolate(Expression("x[0] + 1.0"), P, name="Pressure")
-  z = main(p)
-
-  print "Z.dim(): ", Z.dim()
-  print "P.dim(): ", P.dim()
-  print "p.vector(): ", list(p.vector())
-  print "z.vector(): ", list(z.vector())
+  z = main(u, p)
 
   A = tuple(p.vector())
   B = tuple(Function(z.sub(1)).vector())
-  print "A: ", A
-  print "B: ", B
+  assert A == B # Check for some dolfin bugs that have been fixed
 
-  assert A == B
-
-  assert adjglobals.adjointer.equation_count == 3
+  assert adjglobals.adjointer.equation_count == 5
 
   success = replay_dolfin(tol=0.0, stop=True)
   assert success
@@ -48,8 +42,6 @@ if __name__ == "__main__":
   m = InitialConditionParameter("Pressure")
   Jm = assemble(form(z))
   dJdm = compute_gradient(J, m, forget=False)
-  #dJdm.vector()[0] = 1.7499999999999982
-  #dJdm.vector()[1] = 0.8333333333333324
   print "dJdm:   ", list(dJdm.vector())
 
   eps = 0.0001
@@ -57,14 +49,14 @@ if __name__ == "__main__":
   for i in range(P.dim()):
     p_ptb = Function(p)
     p_ptb.vector()[i] += eps
-    z_ptb = main(p_ptb)
+    z_ptb = main(u, p_ptb)
     J_ptb = assemble(form(z_ptb))
     dJdm_fd.vector()[i] = (J_ptb - Jm)/eps
 
   print "dJdm_fd: ", list(dJdm_fd.vector())
 
   def Jhat(p):
-    z = main(p)
+    z = main(u, p)
     return assemble(form(z))
 
   minconv = taylor_test(Jhat, m, Jm, dJdm, seed=1.0e-3)
