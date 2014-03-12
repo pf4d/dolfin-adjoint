a52bcf40e6ca64be486773307658765c0ddc50f9
Patrick Farrell <patrick.farrell@imperial.ac.uk>
2014-03-12 04:22:07 +0000

    TLM is currently completely wrong .. will fix tomorrow.

--- tests_dolfin/function_assigner/function_assigner.py
+++ tests_dolfin/function_assigner/function_assigner.py
@@ -42,7 +42,6 @@ if __name__ == "__main__":
   m = InitialConditionParameter("Pressure")
   Jm = assemble(form(z))
   dJdm = compute_gradient(J, m, forget=False)
-  print "dJdm:   ", list(dJdm.vector())
 
   eps = 0.0001
   dJdm_fd = Function(P)
@@ -54,6 +53,16 @@ if __name__ == "__main__":
     dJdm_fd.vector()[i] = (J_ptb - Jm)/eps
 
   print "dJdm_fd: ", list(dJdm_fd.vector())
+
+  dJdm_tlm_result = Function(P)
+  dJdm_tlm = compute_gradient_tlm(J, m, forget=False)
+  for i in range(P.dim()):
+    test_vec = Function(P)
+    test_vec.vector()[i] = 1.0
+    dJdm_tlm_result.vector()[i] = dJdm_tlm.inner(test_vec.vector())
+
+  print "dJdm_tlm: ", list(dJdm_tlm_result.vector())
+
 
   def Jhat(p):
     z = main(u, p)
