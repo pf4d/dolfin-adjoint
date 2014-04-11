9488d10d86a220d0574f0cbe9339826f714d2436
Simon Funke <simon.funke@gmail.com>
2014-04-11 21:36:59 +0200

    Update the example to be consistent with the website.

--- examples/optimal_poisson/optimal_poisson.py
+++ examples/optimal_poisson/optimal_poisson.py
@@ -1,10 +1,10 @@
 """ Solves a optimal control problem constrained by the Poisson equation:
 
-    min_(u, m) \int_\Omega 1/2 || u - u_d ||^2
+    min_(u, m) \int_\Omega 1/2 || u - d ||^2 + 1/2 || f ||^2
     
     subjecct to 
 
-    grad \cdot \grad u = m    in \Omega
+    grad \cdot \grad u = f    in \Omega
     u = 0                     on \partial \Omega
 
 
@@ -12,62 +12,55 @@
 from dolfin import *
 from dolfin_adjoint import *
 import moola
-dolfin.set_log_level(ERROR)
 
-
-# Create mesh
+# Create mesh, refined in the center
 n = 64
 mesh = UnitSquareMesh(n, n)
 
-# Refine mesh around the midpoint
 cf = CellFunction("bool", mesh)
 subdomain = CompiledSubDomain('std::abs(x[0]-0.5)<.25 && std::abs(x[1]-0.5)<0.25')
 subdomain.mark(cf, True)
 mesh = refine(mesh, cf)
 
-
 # Define discrete function spaces and funcions
 V = FunctionSpace(mesh, "CG", 1)
 W = FunctionSpace(mesh, "DG", 0)
 
-m = Function(W, name='Control')
+f = Function(W, name='Control')
 u = Function(V, name='State')
 v = TestFunction(V)
 
-
 # Define and solve the Poisson equation to generate the dolfin-adjoint annotation
-F = (inner(grad(u), grad(v)) - m*v)*dx 
+F = (inner(grad(u), grad(v)) - f*v)*dx 
 bc = DirichletBC(V, 0.0, "on_boundary")
 solve(F == 0, u, bc)
 
-
 # Define functional of interest and the reduced functional
 x = triangle.x
-u_d = 1/(2*pi**2)*sin(pi*x[0])*sin(pi*x[1]) # the desired temperature profile
+d = 1/(2*pi**2)*sin(pi*x[0])*sin(pi*x[1]) # the desired temperature profile
 
-J = Functional((0.5*inner(u-u_d, u-u_d))*dx)
-rf = ReducedFunctional(J, InitialConditionParameter(m))
-
+alpha = Constant(1e-2)
+J = Functional((0.5*inner(u-d, u-d))*dx + alpha/2*f**2*dx)
+control = SteadyParameter(f)
+rf = ReducedFunctional(J, control)
 
 # Solve the optimsiation problem with the Moola optimisation package
 problem = rf.moola_problem()
 solver = moola.NewtonCG(options={'gtol': 1e-5, 'maxiter': 20})
-m_moola = moola.DolfinPrimalVector(m)
-sol = solver.solve(problem, m_moola)
-m_opt = sol['Optimizer'].data
+f_moola = moola.DolfinPrimalVector(f)
+sol = solver.solve(problem, f_moola)
+f_opt = sol['Optimizer'].data
 
-plot(m_opt, interactive=True)
-
+plot(f_opt, interactive=True)
 
 # Define the expressions of the analytical solution
-m_analytic = Expression("sin(pi*x[0])*sin(pi*x[1])")
+f_analytic = Expression("sin(pi*x[0])*sin(pi*x[1])")
 u_analytic = Expression("1/(2*pi*pi)*sin(pi*x[0])*sin(pi*x[1])")
 
-
 # Compute errors between numerical and analytical solutions
-m.assign(m_opt)  # Solve the Poisson problem again for the optimal m
+f.assign(f_opt)  # Solve the Poisson problem again for the optimal m
 solve(F == 0, u, bc)  
-control_error = errornorm(m_analytic, m_opt)
+control_error = errornorm(f_analytic, f_opt)
 state_error = errornorm(u_analytic, u)
 print "Error in state: {}.".format(state_error)
 print "Error in control: {}.".format(control_error)
