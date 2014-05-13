6da81925242a76f05cada5ce17696160d116f418
Simon Funke <simon.funke@gmail.com>
2014-05-13 15:47:32 +0200

    Update the Poisson mother example to be consistent with the website

--- examples/poisson-mother/poisson-mother.py
+++ examples/poisson-mother/poisson-mother.py
@@ -12,6 +12,7 @@
 from dolfin import *
 from dolfin_adjoint import *
 import moola
+set_log_level(ERROR)
 
 # Create mesh, refined in the center
 n = 64
@@ -26,7 +27,7 @@ mesh = refine(mesh, cf)
 V = FunctionSpace(mesh, "CG", 1)
 W = FunctionSpace(mesh, "DG", 0)
 
-f = Function(W, name='Control')
+f = interpolate(Expression("x[0]+x[1]"), W, name='Control')
 u = Function(V, name='State')
 v = TestFunction(V)
 
@@ -39,7 +40,7 @@ solve(F == 0, u, bc)
 x = SpatialCoordinate(mesh)
 d = 1/(2*pi**2)*sin(pi*x[0])*sin(pi*x[1]) # the desired temperature profile
 
-alpha = Constant(1e-2)
+alpha = Constant(1e-6)
 J = Functional((0.5*inner(u-d, u-d))*dx + alpha/2*f**2*dx)
 control = SteadyParameter(f)
 rf = ReducedFunctional(J, control)
@@ -47,11 +48,12 @@ rf = ReducedFunctional(J, control)
 # Solve the optimsiation problem with the Moola optimisation package
 problem = rf.moola_problem()
 f_moola = moola.DolfinPrimalVector(f)
-solver = moola.NewtonCG(problem, f_moola, options={'gtol': 1e-5, 'maxiter': 20})
+solver = moola.NewtonCG(problem, f_moola, options={'gtol': 1e-9, 'maxiter': 20, 'display': 3, 'ncg_hesstol': 0})
+#solver = moola.BFGS(problem, f_moola, options={'jtol': 0, 'gtol': 1e-9, 'Hinit': "default", 'maxiter': 100, 'mem_lim': 10})
 sol = solver.solve()
 f_opt = sol['control'].data
 
-plot(f_opt, interactive=True)
+plot(f_opt, interactive=True, title="f_opt")
 
 # Define the expressions of the analytical solution
 f_analytic = Expression("sin(pi*x[0])*sin(pi*x[1])")
