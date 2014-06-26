808fd2c04dee5a95adfd1766af1ad6a3aa00e836
Patrick Farrell <patrick.farrell@imperial.ac.uk>
2014-06-26 10:12:59 +0100

    Add helper functions to en-list and de-list controls/gradients/whatever.

---
+++ dolfin_adjoint/enlisting.py
@@ -0,0 +1,37 @@
+"""
+Handle en-listing and de-listing of controls, gradients, etc.
+
+We want to support lists of controls [m1, m2, ...] to be optimised
+simultaneously. But we also want the user to be able to say
+
+dJ = compute_gradient(J, m)
+
+rather than
+
+dJ = compute_gradient(J, [m])[0]
+
+.
+
+Hence, we store everything internally in lists, but we need to know when
+to de-list (i.e. when the user gave us a single control rather than
+a list.) We do this by subclassing list and providing helper functions
+to en-list and de-list appropriately.
+"""
+
+import collections
+
+class Enlisted(list):
+    pass
+
+def enlist(x):
+    if isinstance(x, collections.Iterable):
+        return x
+    else:
+        return Enlisted([x])
+
+def delist(x):
+    if isinstance(x, Enlisted):
+        assert len(x) == 1
+        return x[0]
+    else:
+        return x
