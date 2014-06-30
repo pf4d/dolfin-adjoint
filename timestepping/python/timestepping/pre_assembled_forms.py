bfc6eb4b8ad8e71ca5c2af34531d818f93847832
James Maddison <jamesmadd@gmail.com>
2014-06-30 17:19:26 +0100

    Documentation and tweaks

--- timestepping/python/timestepping/pre_assembled_forms.py
+++ timestepping/python/timestepping/pre_assembled_forms.py
@@ -111,6 +111,14 @@ def matrix_optimisation(form):
   return mat_form, fn
 
 class PAFilter(object):
+  """
+  A pre-assembly filter. This processes input forms for pre-assembly.
+  
+  Constructor arguments:
+    quadrature_degree: Quadrature degree used for form assembly.
+    pre_assembly_parameters: Parameters defining detailed optimisation options.
+  """
+  
   def __init__(self, quadrature_degree, pre_assembly_parameters):
     if not isinstance(quadrature_degree, int) or quadrature_degree < 0:
       raise InvalidArgumentException("quadrature_degree must be a non-negative integer")
@@ -127,6 +135,10 @@ class PAFilter(object):
     return
   
   def name(self):
+    """
+    Return the name of the PAFilter type.
+    """
+    
     return self.__class__.__name__
   
   def _add(self, form):
@@ -167,31 +179,79 @@ class PAFilter(object):
         form_compiler_parameters = {"quadrature_degree":self._quadrature_degree})
   
   def add(self, form):
+    """
+    Attempt to add a Form to the PAFilter. Return a (form, n_added) pair,
+    where form is the remaining part of the input Form *not* added to the
+    PAFilter, and n_added indicates the number of terms which *are* added to the
+    PAFilter.
+    """
+    
     if not isinstance(form, ufl.form.Form):
       raise InvalidArgumentException("form must be a Form")
-    elif is_empty_form(form):
-      return ufl.form.Form([]), 0
     
-    self._add(form)
-    
-    return form, 1
+    return form, 0
   
   def n(self):
+    """
+    Return the number of terms added to the PAFilter.
+    """
+    
     return self._n
   
   def rank(self):
+    """
+    Return the rank of the Form associated with the PAFilter.
+    """
+    
     return self._rank
   
   def pre_assemble(self):
+    """
+    Pre-assemble the PAFilter.
+    """
+    
     return
   
-  def match_tensor(self, tensor = None, copy = False): 
+  def match_tensor(self, tensor = None): 
+    """
+    Addition of GenericMatrix s can be more efficient if the sparsity patterns
+    match. Extend the sparsity pattern of the input GenericMatrix and any
+    relevant internally stored GenericMatrix so that their sparsity patterns
+    match. Return the expanded GenericMatrix if tensor is not None. Otherwise
+    return a copy of an internal GenericMatrix if one exists, or None otherwise.
+    Must be called after the pre_assemble method.
+    
+    Arguments:
+      tensor: The GenericMatrix whose sparsity pattern is to match any relevant
+              internally stored GenericMatrix s.
+    """
+    
     return tensor
   
   def assemble(self, tensor = None, same_nonzero_pattern = False, copy = False):
+    """
+    Return the result of assembling the Form associated with the PAFilter.
+    
+    Arguments:
+      tensor: If not None, add the result of assembing the form to tensor.
+      same_nonzero_pattern: If tensor is a GenericMatrix, whether tensor has the
+                            same sparsity pattern as the GenericMatrix which
+                            will be added to it.
+      copy: Whether the result should be a copy. If copy is False then the
+            result should not be modified.
+    """
+    
     return None
   
 class PAStaticFilter(PAFilter):
+  """
+  A pre-assembly filter which processes static terms.
+  
+  Constructor arguments:
+    quadrature_degree: Quadrature degree used for form assembly.
+    pre_assembly_parameters: Parameters defining detailed optimisation options.
+  """
+  
   def __init__(self, quadrature_degree, pre_assembly_parameters):
     PAFilter.__init__(self, quadrature_degree, pre_assembly_parameters)
     self.__pre_assembled = None
@@ -199,10 +259,17 @@ class PAStaticFilter(PAFilter):
     return
   
   def add(self, form):
+    """
+    Attempt to add a Form to the PAFilter. Return a (form, n_added) pair,
+    where form is the remaining part of the input Form *not* added to the
+    PAFilter, and n_added indicates the number of terms which *are* added to the
+    PAFilter.
+    """
+    
     if not isinstance(form, ufl.form.Form):
       raise InvalidArgumentException("form must be a Form")
     elif is_empty_form(form):
-      return ufl.form.Form([]), 0
+      return form, 0
     
     if is_static_form(form):
       self._add(form)
@@ -212,6 +279,10 @@ class PAStaticFilter(PAFilter):
       return form, 0
   
   def pre_assemble(self):
+    """
+    Pre-assemble the PAFilter.
+    """
+    
     if self._n == 0:
       return
     
@@ -219,18 +290,32 @@ class PAStaticFilter(PAFilter):
     
     return
   
-  def match_tensor(self, tensor = None, copy = False):
+  def match_tensor(self, tensor = None):
+    """
+    Addition of GenericMatrix s can be more efficient if the sparsity patterns
+    match. Extend the sparsity pattern of the input GenericMatrix and any
+    relevant internally stored GenericMatrix so that their sparsity patterns
+    match. Return the expanded GenericMatrix if tensor is not None. Otherwise
+    return a copy of an internal GenericMatrix if one exists, or None otherwise.
+    Must be called after the pre_assemble method.
+    
+    Arguments:
+      tensor: The GenericMatrix whose sparsity pattern is to match any relevant
+              internally stored GenericMatrix s.
+    """
+    
     if self.__pre_assembled is None:
       if self._n == 0:
-        return None
+        return tensor
       else:
         raise StateException("Cannot call match_tensor method when not pre-assembled")
+    elif not self._rank == 2:
+      raise StateException("Unexpected form rank: %i" % self._rank)
+    if not tensor is None and not isinstance(tensor, dolfin.GenericMatrix):
+      raise InvalidArgumentException("tensor must be a GenericMatrix")
     
     if tensor is None:
-      if copy:
-        return self.__pre_assembled.copy()
-      else:
-        return self.__pre_assembled
+      return self.__pre_assembled.copy()
     else:
       if isinstance(self.__pre_assembled, dolfin.GenericMatrix):
         self.__pre_assembled.axpy(0.0, tensor, False)
@@ -238,6 +323,18 @@ class PAStaticFilter(PAFilter):
       return tensor
   
   def assemble(self, tensor = None, same_nonzero_pattern = False, copy = False):
+    """
+    Return the result of assembling the Form associated with the PAFilter.
+    
+    Arguments:
+      tensor: If not None, add the result of assembing the form to tensor.
+      same_nonzero_pattern: If tensor is a GenericMatrix, whether tensor has the
+                            same sparsity pattern as the GenericMatrix which
+                            will be added to it.
+      copy: Whether the result should be a copy of internal data. If copy is
+            False and tensor is None then the result should not be modified.
+    """
+    
     if self.__pre_assembled is None:
       if self._n == 0:
         return None
@@ -257,6 +354,15 @@ class PAStaticFilter(PAFilter):
       return tensor
  
 class PAMatrixFilter(PAFilter):
+  """
+  A pre-assembly filter which processes terms which can be converted to a
+  static bi-linear form action.
+  
+  Constructor arguments:
+    quadrature_degree: Quadrature degree used for form assembly.
+    pre_assembly_parameters: Parameters defining detailed optimisation options.
+  """
+  
   def __init__(self, quadrature_degree, pre_assembly_parameters):
     PAFilter.__init__(self, quadrature_degree, pre_assembly_parameters)
     self.__L = OrderedDict()
@@ -265,10 +371,17 @@ class PAMatrixFilter(PAFilter):
     return
   
   def add(self, form):
+    """
+    Attempt to add a Form to the PAFilter. Return a (form, n_added) pair,
+    where form is the remaining part of the input Form *not* added to the
+    PAFilter, and n_added indicates the number of terms which *are* added to the
+    PAFilter.
+    """
+    
     if not isinstance(form, ufl.form.Form):
       raise InvalidArgumentException("form must be a Form")
     elif is_empty_form(form):
-      return ufl.form.Form([]), 0
+      return form, 0
   
     mform = matrix_optimisation(form)
     if mform is None:
@@ -284,6 +397,10 @@ class PAMatrixFilter(PAFilter):
     return ufl.form.Form([]), 1
   
   def pre_assemble(self):
+    """
+    Pre-assemble the PAFilter.
+    """
+    
     if self._n == 0:
       return
     
@@ -294,6 +411,18 @@ class PAMatrixFilter(PAFilter):
     return
   
   def assemble(self, tensor = None, same_nonzero_pattern = False, copy = False):
+    """
+    Return the result of assembling the Form associated with the PAFilter.
+    
+    Arguments:
+      tensor: If not None, add the result of assembing the form to tensor.
+      same_nonzero_pattern: If tensor is a GenericMatrix, whether tensor has the
+                            same sparsity pattern as the GenericMatrix which
+                            will be added to it.
+      copy: Whether the result should be a copy of internal data. If copy is
+            False and tensor is None then the result should not be modified.
+    """
+    
     if self.__pre_assembled is None:
       if self._n == 0:
         return None
@@ -312,6 +441,15 @@ class PAMatrixFilter(PAFilter):
       return tensor
   
 class NonPAFilter(PAFilter):
+  """
+  A dummy pre-assembly filter which accepts any Form, and uses standard
+  assembly.
+  
+  Constructor arguments:
+    quadrature_degree: Quadrature degree used for form assembly.
+    pre_assembly_parameters: Parameters defining detailed optimisation options.
+  """
+  
   def __init__(self, quadrature_degree, pre_assembly_parameters):
     PAFilter.__init__(self, quadrature_degree, pre_assembly_parameters)
     self.__tensor = None
@@ -319,18 +457,42 @@ class NonPAFilter(PAFilter):
     return
   
   def add(self, form):
+    """
+    Attempt to add a Form to the PAFilter. Return a (form, n_added) pair,
+    where form is the remaining part of the input Form *not* added to the
+    PAFilter, and n_added indicates the number of terms which *are* added to the
+    PAFilter.
+    """
+    
     if not isinstance(form, ufl.form.Form):
       raise InvalidArgumentException("form must be a Form")
     elif is_empty_form(form):
-      return ufl.form.Form([]), 0
+      return form, 0
     
     self._add(form)
     self.__tensor = None
     return ufl.form.Form([]), 1
   
-  def match_tensor(self, tensor, copy = False):
+  def match_tensor(self, tensor):
+    """
+    Addition of GenericMatrix s can be more efficient if the sparsity patterns
+    match. Extend the sparsity pattern of the input GenericMatrix and any
+    relevant internally stored GenericMatrix so that their sparsity patterns
+    match. Return the expanded GenericMatrix if tensor is not None. Otherwise
+    return a copy of an internal GenericMatrix if one exists, or None otherwise.
+    Must be called after the pre_assemble method.
+    
+    Arguments:
+      tensor: The GenericMatrix whose sparsity pattern is to match any relevant
+              internally stored GenericMatrix s.
+    """
+    
     if self._n == 0:
       return tensor
+    elif not self._rank == 2:
+      raise StateException("Unexpected form rank: %i" % self._rank)
+    if not tensor is None and not isinstance(tensor, dolfin.GenericMatrix):
+      raise InvalidArgumentException("tensor must be a GenericMatrix")
     
     one = dolfin.Constant(1.0)
     form = dolfin.replace(self._form, {c:one for c in ufl.algorithms.extract_coefficients(self._form)})    
@@ -347,6 +509,18 @@ class NonPAFilter(PAFilter):
     return tensor
   
   def assemble(self, tensor = None, same_nonzero_pattern = False, copy = False):
+    """
+    Return the result of assembling the Form associated with the PAFilter.
+    
+    Arguments:
+      tensor: If not None, add the result of assembing the form to tensor.
+      same_nonzero_pattern: If tensor is a GenericMatrix, whether tensor has the
+                            same sparsity pattern as the GenericMatrix which
+                            will be added to it.
+      copy: Whether the result should be a copy of internal data. If copy is
+            False and tensor is None then the result should not be modified.
+    """
+    
     if self._n == 0:
       return tensor
     
@@ -373,6 +547,15 @@ class NonPAFilter(PAFilter):
         return tensor
 
 class PAForm(object):
+  """
+  A pre-assembled form. Given a form of arbitrary rank, this finds and
+  pre-assembles static terms.
+
+  Constructor arguments:
+    form: The Form to be pre-assembled.
+    pre_assembly_parameters: Parameters defining detailed optimisation options.
+  """
+  
   def __init__(self, form, pre_assembly_parameters = {}):
     if not isinstance(form, ufl.form.Form):
       raise InvalidArgumentException("form must be a Form")
@@ -448,22 +631,20 @@ class PAForm(object):
     filters = [non_pa_filter] + pa_filters
     for filter in copy.copy(filters):
       n_filter = filter.n()
+      n[filter.name()] = n_filter
       if isinstance(filter, NonPAFilter):
         n_non_pa += n_filter
       else:
         n_pa += n_filter
-      n[filter.name()] = filter.n()
       if filter.n() == 0:
         filters.remove(filter)
       else:
         filter.pre_assemble()
     
     if self.__rank == 2 and len(filters) > 1:
-      tensor = filters[0].match_tensor(copy = True)
-      for filter in filters[1:]:
-        tensor = filter.match_tensor(tensor = tensor, copy = False)
-      for filter in filters[:-1]:
-        tensor = filter.match_tensor(tensor = tensor, copy = False)
+      tensor = filters[0].match_tensor()
+      for filter in filters[1:] + filters[:-1]:
+        tensor = filter.match_tensor(tensor = tensor)
       same_nonzero_pattern = True
     else:
       same_nonzero_pattern = False
@@ -477,6 +658,14 @@ class PAForm(object):
     return
 
   def assemble(self, copy = False):
+    """
+    Return the result of assembling the Form associated with the PAForm. 
+    
+    Arguments:
+      copy: Whether the result should be a copy of internal data. If copy is
+            False then the result should not be modified.
+    """
+    
     if len(self.__filters) == 0:
       raise StateException("Cannot assemble empty form")
     elif len(self.__filters) == 1:
@@ -488,21 +677,46 @@ class PAForm(object):
       return tensor
     
   def rank(self):
+    """
+    Return the PAForm rank.
+    """
+    
     return self.__rank
   
   def is_static(self):
+    """
+    Return whether the PAForm is static.
+    """
+    
     return self.__is_static
   
   def n(self):
+    """
+    Return a dictionary of name:n_added pairs, indicating the number of terms
+    added to each considered PAFilter.
+    """
+    
     return self.__n
     
   def n_pre_assembled(self):
+    """
+    Return the number of pre-assembled terms.
+    """
+    
     return self.__n_pre_assembled
 
   def n_non_pre_assembled(self):
+    """
+    Return the number of non-pre-assembled terms.
+    """
+    
     return self.__n_non_pre_assembled
 
   def dependencies(self, non_symbolic = False):
+    """
+    Return PAForm dependencies. The optional non_symbolic has no effect.
+    """
+    
     return self.__deps
   
 _assemble_classes.append(PAForm)
