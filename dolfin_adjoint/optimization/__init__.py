7ee8a8e44b062eb49e2bd20fa36c267f2dab0c2e
Patrick Farrell <patrick.farrell@imperial.ac.uk>
2014-06-30 07:15:14 +0100

    imperial -> maths.ox

--- dolfin_adjoint/optimization/__init__.py
+++ dolfin_adjoint/optimization/__init__.py
@@ -2,9 +2,9 @@
 The optimization module is automatically imported by dolfin-adjoint 
 """
 
-__version__ = '1.1.0+'
+__version__ = '1.4'
 __author__  = 'Patrick Farrell and Simon Funke'
 __credits__ = ['Patrick Farrell', 'Simon Funke', 'David Ham', 'Marie Rognes']
 __license__ = 'LGPL-3'
 __maintainer__ = 'Simon Funke'
-__email__ = 's.funke09@imperial.ac.uk'
+__email__ = 'simon@simula.no'
