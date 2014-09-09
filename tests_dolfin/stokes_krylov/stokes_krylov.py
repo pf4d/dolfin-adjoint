029ec2c6bbab6a0474a8b8cc3cb0bf54400ee1fb
Simon Funke <simon.funke@gmail.com>
2014-09-09 14:10:00 +0200

    Make stokes_krylov test more robust

--- tests_dolfin/stokes_krylov/stokes_krylov.py
+++ tests_dolfin/stokes_krylov/stokes_krylov.py
@@ -85,7 +85,7 @@ A, bb = assemble_system(a, L, bcs)
 P, btmp = assemble_system(b, L, bcs)
 
 # Create Krylov solver and AMG preconditioner
-solver = KrylovSolver("tfqmr", "amg")
+solver = KrylovSolver("tfqmr", "default")
 
 # Associate operator (A) and preconditioner matrix (P)
 solver.set_operators(A, P)
