bcbe3e7c89bdd2e781f7e498c316a96f71213684
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-01-07 21:25:07 +0000

    Added debugging statements and trying to fix the numpy.dot issue for mixed functions.

--- dolfin_adjoint/utils.py
+++ dolfin_adjoint/utils.py
@@ -565,7 +565,11 @@ def taylor_test(J, m, Jm, dJdm, HJm=None, seed=None, perturbation_direction=None
       if backend.__name__  == "dolfin":
         remainder = abs(functional_values[i] - Jm - dJdm.vector().inner(perturbations[i].vector()))
       else:
-        remainder = abs(functional_values[i] - Jm - numpy.dot(dJdm.vector().array(), perturbations[i].vector().array()))
+        total = 0
+        for j in range(len(dJdm.vector().array()[0])):
+           total += numpy.dot(dJdm.vector().array()[0][j], perturbations[i].vector().array()[0][j])
+        total += numpy.dot(dJdm.vector().array()[1], perturbations[i].vector().array()[1])
+        remainder = abs(functional_values[i] - Jm - total)
       with_gradient.append(remainder)
 
   if min(with_gradient + no_gradient) < 1e-16:
