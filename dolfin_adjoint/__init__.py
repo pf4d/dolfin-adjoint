50b246708b60f3a82c5a99e97d7180b3dbffcb5b
Simon Funke <simon.funke@gmail.com>
2015-01-16 17:12:31 +0100

    Update version numbers

--- dolfin_adjoint/__init__.py
+++ dolfin_adjoint/__init__.py
@@ -13,7 +13,7 @@ importing the :py:mod:`dolfin` module. dolfin-adjoint relies on *overloading* ma
 the key functions of dolfin to achieve its degree of automation.
 """
 
-__version__ = '1.4'
+__version__ = '1.5'
 __author__  = 'Patrick Farrell and Simon Funke'
 __credits__ = ['Patrick Farrell', 'Simon Funke', 'David Ham', 'Marie Rognes']
 __license__ = 'LGPL-3'
