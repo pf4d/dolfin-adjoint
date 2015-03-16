d4e38d4cf35f288a1d6b7029bad46015b7aa041d
Steven Vandekerckhove <Steven.Vandekerckhove@kuleuven.be>
2015-03-16 15:47:15 +0100

    nodal_functional working for finish_time (default)

--- dolfin_adjoint/nodal_functional.py
+++ dolfin_adjoint/nodal_functional.py
@@ -14,8 +14,9 @@ from timeforms import NoTime, StartTimeConstant, FinishTimeConstant, dt, FINISH_
 from IPython import embed
 
 class NodalFunctional(functional.Functional):
-    def __init__(self, u, refs, times, coords, timeform=False, verbose=False, name=None):
-
+    def __init__(self, u, refs, coords, times=None, timeform=False, verbose=False, name=None):
+        if times is None:
+            times = ["FINISH_TIME"]
         # we prepare a ghost timeform. Only the time instant is important
         if not timeform: self.timeform = sum(u*dx*dt[t] for t in times)
         else: self.timeform = timeform
@@ -27,17 +28,24 @@ class NodalFunctional(functional.Functional):
         self.times = times
 
     def __call__(self, adjointer, timestep, dependencies, values):
+        print "eval ", len(values)
         toi = _time_levels(adjointer, timestep)[0]
-        if toi in self.times and len(values) > 0:
-            solu = values[-1].data(self.coords)
-            ref  = self.refs[self.times.index(toi)]
-            my = (solu - float(ref))*(solu - float(ref))
-
-            if len(values) > 1:
-                toi = self.times[-1]
+        if len(values) > 0:
+            if timestep is adjointer.timestep_count -1:
+                # add final contribution
                 solu = values[0].data(self.coords)
+                ref  = self.refs[self.times.index(self.times[-1])]
+                my = (solu - float(ref))*(solu - float(ref))
+
+                # if necessary, add one but last contribution
+                if toi in self.times and len(values) > 0:
+                    solu = values[-1].data(self.coords)
+                    ref  = self.refs[self.times.index(toi)]
+                    my += (solu - float(ref))*(solu - float(ref))
+            else:
+                solu = values[-1].data(self.coords)
                 ref  = self.refs[self.times.index(toi)]
-                my += (solu - float(ref))*(solu - float(ref))
+                my = (solu - float(ref))*(solu - float(ref))
         else:
             my = 0.0
 
@@ -60,26 +68,31 @@ class NodalFunctional(functional.Functional):
         return my
 
     def derivative(self, adjointer, variable, dependencies, values):
+        #transate finish_time: UGLY!!
+        if "FINISH_TIME" in self.times:
+            final_time = _time_levels(adjointer, adjointer.timestep_count - 1)[1]
+            self.times[self.times.index("FINISH_TIME")] = final_time
+
         print "derive ", variable.timestep, " num values ", len(values)
         timesteps = self._derivative_timesteps(adjointer, variable)
-        if len(values) is 1: # one value (easy)
+        if len(timesteps) is 1: # only occurs at atart and finish time
             tsoi = timesteps[-1]
-            toi = _time_levels(adjointer, tsoi)[0]
-            ind = 0
-        elif len(values) is 2: # two values (hard)
-            tsoi = timesteps[-1]
-            if len(timesteps) is 1: # only occurs at atart and finish time
-                if tsoi is 0: toi = _time_levels(adjointer, tsoi)[0]; ind = 1
-                else: toi = _time_levels(adjointer, tsoi)[-1]; ind = 0
-            else:
+            if tsoi is 0: toi = _time_levels(adjointer, tsoi)[0]; ind = -1
+            else: toi = _time_levels(adjointer, tsoi)[-1]; ind = 0
+        else:
+            if len(values) is 1: # one value (easy)
+                tsoi = timesteps[-1]
+                toi = _time_levels(adjointer, tsoi)[0]
+                ind = 0
+            elif len(values) is 2: # two values (hard)
+                tsoi = timesteps[-1]
                 toi = _time_levels(adjointer, tsoi)[0]
                 if _time_levels(adjointer, tsoi)[1] in self.times: ind = 0
                 else: ind = 1
-        else: # three values (easy)
-            tsoi = timesteps[1]
-            toi = _time_levels(adjointer, tsoi)[0]
-            ind = 1
-
+            else: # three values (easy)
+                tsoi = timesteps[1]
+                toi = _time_levels(adjointer, tsoi)[0]
+                ind = 1
         coef = values[ind].data
         ref  = self.refs[self.times.index(toi)]
         solu = coef(self.coords)
