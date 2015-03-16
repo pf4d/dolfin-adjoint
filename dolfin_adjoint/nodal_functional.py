b7064fc448c1cd064e50974e5dc22542464d2eba
Steven Vandekerckhove <Steven.Vandekerckhove@kuleuven.be>
2015-03-16 11:44:33 +0100

    working nodal functional with term in every step

--- dolfin_adjoint/nodal_functional.py
+++ dolfin_adjoint/nodal_functional.py
@@ -14,13 +14,13 @@ from timeforms import NoTime, StartTimeConstant, FinishTimeConstant, dt, FINISH_
 from IPython import embed
 
 class NodalFunctional(functional.Functional):
-    def __init__(self, u, refs, times, coords, verbose=False, name=None):
+    def __init__(self, timeform, u, refs, times, coords, verbose=False, name=None):
 
-        combined = zip(times, refs)
-        I = sum(inner(u - u_obs, u - u_obs)*ds(1)*dt[t]
-                       for (t, u_obs) in combined)
+#        combined = zip(times, refs)
+#        I = sum(inner(u - u_obs, u - u_obs)*ds(1)*dt[t]
+#                       for (t, u_obs) in combined)
 
-        self.timeform = I
+        self.timeform = timeform
         self.coords = coords
         self.verbose = verbose
         self.name = name
@@ -29,6 +29,7 @@ class NodalFunctional(functional.Functional):
         self.times = times
 
     def __call__(self, adjointer, timestep, dependencies, values):
+        print "eval ", timestep, " times ", _time_levels(adjointer, timestep)
         functional_value = self._substitute_form(adjointer, timestep, dependencies, values)
 
         if functional_value is not None:
@@ -37,22 +38,32 @@ class NodalFunctional(functional.Functional):
                 backend.info_red("The form passed into Functional must be rank-0 (a scalar)! You have passed in a rank-%s form." % len(args))
                 raise libadjoint.exceptions.LibadjointErrorInvalidInputs
 
-            if functional_value.coefficients()[0].element().family() is 'Real':
-                solu = functional_value.coefficients()[1](self.coords)
-                ref  = functional_value.coefficients()[0](self.coords)
-            else:
-                solu = functional_value.coefficients()[0](self.coords)
-                ref  = functional_value.coefficients()[1](self.coords)
-            print "da eval ", backend.assemble(functional_value)
-            print "my eval ", (solu - float(ref))*(solu - float(ref))
-            return (solu - float(ref))*(solu - float(ref))
+#            embed()
+
+            toi = _time_levels(adjointer, timestep)[0]
+            solu = values[-1].data(self.coords)
+            ref  = self.refs[self.times.index(toi)]
+            my = (solu - float(ref))*(solu - float(ref))
+
+            if len(values) > 1:
+                toi = self.times[-1]
+                solu = values[0].data(self.coords)
+                ref  = self.refs[self.times.index(toi)]
+                my += (solu - float(ref))*(solu - float(ref))
+
+            da = backend.assemble(functional_value)
+
+            print "da eval ", da
+            print "my eval ", my
+            if abs(da - my) > 1e-13: embed()
+            return my
         else:
             return 0.0
 
     def derivative(self, adjointer, variable, dependencies, values):
         functional_value = None
         for timestep in self._derivative_timesteps(adjointer, variable):
-          print timestep
+          print "derive ", timestep, " times ", _time_levels(adjointer, timestep)
           functional_value = _add(functional_value,
                                   self._substitute_form(adjointer, timestep, dependencies, values))
 
@@ -62,131 +73,35 @@ class NodalFunctional(functional.Functional):
 
         d = backend.derivative(functional_value, values[dependencies.index(variable)].data)
         d = ufl.algorithms.expand_derivatives(d)
-        import IPython; IPython.embed()
 
-        if len(values)>1:
-            coef = values[-2].data
-            ref  = self.timeform.terms[variable.timestep+1].form.coefficients()[1]
+        tsoi = self._derivative_timesteps(adjointer, variable)[0]
+        toi = _time_levels(adjointer, tsoi)[-1]
+        if toi in self.times:
+            "showtime"
+            if len(values) > 1: # occurs for timestep 0
+                coef = values[-2].data
+            else :
+                coef = values[0].data
+                toi = _time_levels(adjointer, tsoi)[0]
+
+            ref  = self.refs[self.times.index(toi)]
+            solu = coef(self.coords)
+            ff = backend.Constant(2.0*(solu - float(ref)))
         else:
-            coef = values[0].data
-            ref  = self.timeform.terms[variable.timestep].form.coefficients()[1]
+            ff = 0.0
 
-        solu = coef(self.coords)
-        ff = Constant(2.0*(solu - float(ref)))
-
-
-        v = project(ff*PointwiseEvaluator(self.coords), coef.function_space())
+        v = backend.project(ff*PointwiseEvaluator(self.coords), coef.function_space())
+        da = backend.assemble(d).array()[0]
+        my = v.vector().array()[0]
         print "step", variable.timestep
-        print "da", assemble(d).array()[0]
-        print "my", v.vector().array()[0]
+        print "da", da
+        print "my", my
+#        embed()
+        if abs(da - my) > 1e-13: embed()
 
-        if abs(assemble(d).array()[0]-v.vector().array()[0]) > 1e-13: import IPython; IPython.embed()
+        if abs(backend.assemble(d).array()[0]-v.vector().array()[0]) > 1e-13: import IPython; IPython.embed()
 
         return adjlinalg.Vector(v)
-
-    def _substitute_form(self, adjointer, timestep, dependencies, values):
-        ''' Perform the substitution of the dependencies and values
-        provided. This is common to __call__ and __derivative__'''
-
-        deps = {}
-        for dep, val in zip(dependencies, values):
-          deps[str(dep)] = val.data
-
-        functional_value = None
-        final_time = _time_levels(adjointer, adjointer.timestep_count - 1)[1]
-
-        # Get the necessary timestep information about the adjointer.
-        # For integrals, we're integrating over /two/ timesteps.
-        timestep_start, timestep_end = _time_levels(adjointer, timestep)
-        point_interval = slice(timestep_start, timestep_end)
-
-        embed()
-        if timestep > 0:
-          prev_start, prev_end = _time_levels(adjointer, timestep - 1)
-          integral_interval = slice(prev_start, timestep_end)
-        else:
-          integral_interval = slice(timestep_start, timestep_end)
-
-        for term in self.timeform.terms:
-          if isinstance(term.time, slice):
-            # Integral.
-
-            def trapezoidal(interval, iteration):
-              # Evaluate the trapezoidal rule for the given interval and
-              # iteration. Iteration is used to select start and end of timestep.
-              this_interval=_slice_intersect(interval, term.time)
-              if this_interval:
-                # Get adj_variables for dependencies. Time level is not yet specified.
-
-                # Dependency replacement dictionary.
-                replace={}
-
-                term_deps = _coeffs(adjointer, term.form)
-                term_vars = _vars(adjointer, term.form)
-
-                for term_dep, term_var in zip(term_deps,term_vars):
-                  this_var = self.get_vars(adjointer, timestep, term_var)[iteration]
-                  replace[term_dep] = deps[str(this_var)]
-
-                # Trapezoidal rule over given interval.
-                quad_weight = 0.5*(this_interval.stop-this_interval.start)
-
-                return backend.replace(quad_weight*term.form, replace)
-
-            # Calculate the integral contribution from the previous time level.
-            functional_value = _add(functional_value, trapezoidal(integral_interval, 0))
-
-            # On the final occasion, also calculate the contribution from the
-            # current time level.
-            if adjointer.finished and timestep == adjointer.timestep_count - 1: # we're at the end, and need to add the extra terms
-                                                                                # associated with that
-              final_interval = slice(timestep_start, timestep_end)
-              functional_value = _add(functional_value, trapezoidal(final_interval, 1))
-
-          else:
-            # Point evaluation.
-
-            if point_interval.start < term.time < point_interval.stop:
-              replace = {}
-
-              term_deps = _coeffs(adjointer, term.form)
-              term_vars = _vars(adjointer, term.form)
-
-              for term_dep, term_var in zip(term_deps, term_vars):
-                (start, end) = self.get_vars(adjointer, timestep, term_var)
-                theta = 1.0 - (term.time - point_interval.start)/(point_interval.stop - point_interval.start)
-                replace[term_dep] = theta*deps[str(start)] + (1-theta)*deps[str(end)]
-
-              functional_value = _add(functional_value, backend.replace(term.form, replace))
-
-            # Special case for evaluation at the end of time: we can't pass over to the
-            # right-hand timestep, so have to do it here.
-            elif (term.time == final_time or isinstance(term.time, FinishTimeConstant)) and point_interval.stop == final_time:
-              replace = {}
-
-              term_deps = _coeffs(adjointer, term.form)
-              term_vars = _vars(adjointer, term.form)
-
-              for term_dep, term_var in zip(term_deps, term_vars):
-                end = self.get_vars(adjointer, timestep, term_var)[1]
-                replace[term_dep] = deps[str(end)]
-
-              functional_value = _add(functional_value, backend.replace(term.form, replace))
-
-            # Another special case for the start of a timestep.
-            elif (isinstance(term.time, StartTimeConstant) and timestep == 0) or point_interval.start == term.time:
-              replace = {}
-
-              term_deps = _coeffs(adjointer, term.form)
-              term_vars = _vars(adjointer, term.form)
-
-              for term_dep, term_var in zip(term_deps, term_vars):
-                end = self.get_vars(adjointer, timestep, term_var)[0]
-                replace[term_dep] = deps[str(end)]
-
-              functional_value = _add(functional_value, backend.replace(term.form, replace))
-
-        return functional_value
 
 class PointwiseEvaluator(backend.Expression):
     def __init__(self, X):
