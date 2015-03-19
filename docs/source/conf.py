a560716c3ed72dafcd8ae165ad292edc70d7342a
Simon Funke <simon.funke@gmailcom>
2015-03-19 18:51:00 +0100

    Add ufl.algorithms to Mock class

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ class Mock(object):
             return Mock()
 
 MOCK_MODULES = ['dolfin', 'ufl', 'numpy', 'scipy', 'scipy.optimize',
-                'ufl.classes']
+                'ufl.classes', 'ufl.algorithms']
 for mod_name in MOCK_MODULES:
     try:
         importlib.import_module(mod_name)
