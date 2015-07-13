96305133f00be864ec28506c26bee90f30c2f405
Simon Funke <simon.funke@gmail.com>
2015-07-13 13:22:08 +0200

    Never plot when in tests

--- tests_dolfin/pointwise_functional/pointwise_functional.py
+++ tests_dolfin/pointwise_functional/pointwise_functional.py
@@ -37,7 +37,7 @@ solve(a == L, u_, bc)
 J = Functional(u_*dPP(1) + u_*dx)
 Jhat = ReducedFunctional(J, Control(f))
 
-dJ = compute_gradient(J, Control(f))
-plot(dJ, interactive=True, title="dJ")
+dJ = compute_gradient(J, Control(f), forget=False)
+#plot(dJ, interactive=True, title="dJ")
 
 Jhat.derivative(project=True)
